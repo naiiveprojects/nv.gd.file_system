f12d0808779aebdcd366384a2a55649da0c63825
illlustr <still.lkmn@hotmail.com>
2023-10-07 15:27:57 +0700

    4.x

--- addons/nv.file_system/plugin.gd
+++ addons/nv.file_system/plugin.gd
@@ -1,4 +1,4 @@
-tool
+@tool
 extends EditorPlugin
 
 const TITLE := "File System"
@@ -19,7 +19,7 @@ var _processing: bool = false
 var file_system: Control
 
 ## FileSystem Tool button from : `add_control_to_bottom_panel()`
-var file_system_button: ToolButton
+var file_system_button: Button
 
 ## Box container H/V
 var box_container: Control
@@ -33,10 +33,13 @@ var docked: bool = false
 
 func _enter_tree() -> void:
 	# wait until editor fully initialize
-	yield(get_tree(), "idle_frame")
+	await get_tree().process_frame
 	
 	# add switch / toggle to control FileSystem docking position
-	add_tool_menu_item(TITLE_TOOL_MENU_ITEM, self, "switch_file_system_dock")
+	add_tool_menu_item(TITLE_TOOL_MENU_ITEM, switch_file_system_dock)
+	
+	# Get file system
+	file_system = get_editor_interface().get_file_system_dock()
 	
 	docked = false
 	_processing = false
@@ -54,7 +57,8 @@ func _exit_tree() -> void:
 	# Setup vertical container
 	box_container = VBoxContainer.new()
 	split_container = VSplitContainer.new()
-	file_system.rect_min_size = Vector2.ONE
+	file_system.custom_minimum_size = Vector2.ONE
+	
 	# Refrences
 	var headder: Control = file_system.get_child(0) # BoxContainer
 	var body: Control = file_system.get_child(3) # SplitContainer
@@ -67,14 +71,13 @@ func _exit_tree() -> void:
 	var split_button: Button = box_container.get_child(0).get_child(4)
 	var file_tree: Tree = split_container.get_child(0)
 	var file_item: VBoxContainer = split_container.get_child(1)
-	var item_view: ToolButton = file_item.get_child(0).get_child(2)
+	var item_view: Button = file_item.get_child(0).get_child(2)
 	
 	# adjustment
 	split_button.visible = true
-	split_button.pressed = false
-	item_view.pressed = false
+	split_button.button_pressed = false
+	item_view.button_pressed = false
 	
-	file_tree.size_flags_stretch_ratio = TREE_STRETCH_RATIO
 	file_tree.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	file_item.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	
@@ -86,51 +89,40 @@ func _exit_tree() -> void:
 		item.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 
 
-func switch_file_system_dock(_value = null) -> void:
+func switch_file_system_dock() -> void:
 	if _processing:
 		return
 	
 	_processing = true
 	
-	if !file_system:
-		# Get file system
-		file_system = get_editor_interface().get_file_system_dock()
-		yield(get_tree(), "idle_frame")
-		
-		if file_system == null:
-			print_debug("Cant Find FileSystemDock")
-			_processing = false
-			return
-	
 	if !docked:
+		docked = true
 		# Move file system to bottom panel
 		remove_control_from_docks(file_system)
-		
-		yield(get_tree(), "idle_frame")
+		await get_tree().process_frame
 		file_system_button = add_control_to_bottom_panel(file_system, TITLE)
 		
 		# Setup horizontal container
 		box_container = HBoxContainer.new()
 		split_container = HSplitContainer.new()
-		file_system.rect_min_size = Vector2.ONE * MIN_SIZE
+		file_system.custom_minimum_size = Vector2.ONE * MIN_SIZE
 		
 		# Move file button
 		file_system_button.get_parent().move_child(file_system_button, FILE_BUTTON_INDEX)
-		docked = true
+		await get_tree().process_frame
+		file_system_button.button_pressed = docked
 	
 	else:
+		docked = false
 		# Move file system to left panel
 		remove_control_from_bottom_panel(file_system)
-		
-		yield(get_tree(), "idle_frame")
+		await get_tree().process_frame
 		add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, file_system)
 		
 		# Setup vertical container
 		box_container = VBoxContainer.new()
 		split_container = VSplitContainer.new()
-		file_system.rect_min_size = Vector2.ONE
-		
-		docked = false
+		file_system.custom_minimum_size = Vector2.ONE
 	
 	# Refrences
 	var headder: Control = file_system.get_child(0) # BoxContainer
@@ -144,12 +136,12 @@ func switch_file_system_dock(_value = null) -> void:
 	var split_button: Button = box_container.get_child(0).get_child(4)
 	var file_tree: Tree = split_container.get_child(0)
 	var file_item: VBoxContainer = split_container.get_child(1)
-	var item_view: ToolButton = file_item.get_child(0).get_child(2)
+	var item_view: Button = file_item.get_child(0).get_child(2)
 	
 	# adjustment
 	split_button.visible = !docked
-	split_button.pressed = docked
-	item_view.pressed = !docked
+	split_button.button_pressed = docked
+	item_view.button_pressed = docked
 	
 	file_tree.size_flags_stretch_ratio = TREE_STRETCH_RATIO
 	file_tree.size_flags_horizontal = Control.SIZE_EXPAND_FILL
