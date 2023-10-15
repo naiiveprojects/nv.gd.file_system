f6a4d6c306f8a9e0ea0458975b23cf393d0b455b
illlustr <still.lkmn@hotmail.com>
2023-10-15 08:01:52 +0700

    ref

--- addons/nv.file_system/plugin.gd
+++ addons/nv.file_system/plugin.gd
@@ -16,16 +16,23 @@ const FILE_BUTTON_INDEX := 0
 var _processing: bool = false
 
 ## The Editor FileSystem
-var file_system: Control
+var file_system: FileSystemDock
+
+var file_system_box: Control
+var file_system_vbox: VBoxContainer
+var file_system_hbox: HBoxContainer
+
+var file_system_split: Control
+var file_system_vsplit: VSplitContainer
+var file_system_hsplit: HSplitContainer
+
+var file_system_box_split_view: Button
+var file_system_split_tree: Tree
+var file_system_split_item: VBoxContainer
+var file_system_split_item_view: ToolButton
 
 ## FileSystem Tool button from : `add_control_to_bottom_panel()`
 var file_system_button: ToolButton
-
-## Box container H/V
-var box_container: Control
-
-## split container H/V
-var split_container: Control
 
 ## Switch
 var docked: bool = false
@@ -40,10 +47,23 @@ func _enter_tree() -> void:
 	
 	# Get file system
 	file_system = get_editor_interface().get_file_system_dock()
+	file_system_box = file_system.get_child(0) # BoxContainer
+	file_system_split = file_system.get_child(3) # SplitContainer
 	
-	docked = false
-	_processing = false
-	switch_file_system_dock()
+	file_system_vbox = file_system.get_child(0)
+	file_system_vsplit = file_system.get_child(3)
+	
+	file_system_hbox = HBoxContainer.new()
+	file_system_hsplit = HSplitContainer.new()
+	
+	file_system_box_split_view = file_system_vbox.get_child(0).get_child(4)
+	file_system_split_tree = file_system_vsplit.get_child(0)
+	file_system_split_item = file_system_vsplit.get_child(1)
+	file_system_split_item_view = file_system_split_item.get_child(0).get_child(2)
+	
+	#print()
+	
+	call_deferred("switch_file_system_dock")
 
 
 func _exit_tree() -> void:
@@ -53,108 +73,57 @@ func _exit_tree() -> void:
 	if !docked:
 		return
 	
-	# Move file system to left panel
-	remove_control_from_bottom_panel(file_system)
-	add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, file_system)
-	
-	# Setup vertical container
-	box_container = VBoxContainer.new()
-	split_container = VSplitContainer.new()
-	file_system.rect_min_size = Vector2.ONE
-	
-	# Refrences
-	var headder: Control = file_system.get_child(0) # BoxContainer
-	var body: Control = file_system.get_child(3) # SplitContainer
-	
-	# Apply new container
-	headder.replace_by(box_container, true)
-	body.replace_by(split_container, true)
-	
-	# Child Node Refrences
-	var split_button: Button = box_container.get_child(0).get_child(4)
-	var file_tree: Tree = split_container.get_child(0)
-	var file_item: VBoxContainer = split_container.get_child(1)
-	var item_view: ToolButton = file_item.get_child(0).get_child(2)
-	
-	# adjustment
-	split_button.visible = true
-	split_button.pressed = false
-	item_view.pressed = false
-	
-	file_tree.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	file_item.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	
-	box_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	split_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	split_container.size_flags_vertical = Control.SIZE_EXPAND_FILL
-	
-	for item in box_container.get_children():
-		item.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	switch_file_system_dock()
 
 
 func switch_file_system_dock(_value = null) -> void:
 	if _processing:
 		return
-	
 	_processing = true
 	
 	if !docked:
 		docked = true
 		# Move file system to bottom panel
 		remove_control_from_docks(file_system)
-		yield(get_tree(), "idle_frame")
 		file_system_button = add_control_to_bottom_panel(file_system, TITLE)
-		
-		# Setup horizontal container
-		box_container = HBoxContainer.new()
-		split_container = HSplitContainer.new()
 		file_system.rect_min_size = Vector2.ONE * MIN_SIZE
 		
+		# Apply new container
+		file_system_box.replace_by(file_system_hbox, true)
+		file_system_split.replace_by(file_system_hsplit, true)
+		
 		# Move file button
-		file_system_button.get_parent().move_child(file_system_button, FILE_BUTTON_INDEX)
-		yield(get_tree(), "idle_frame")
+		file_system_button.get_parent().call_deferred(
+				"move_child",
+				file_system_button,
+				FILE_BUTTON_INDEX
+		)
 		file_system_button.pressed = true
 	
 	else:
 		docked = false
 		# Move file system to left panel
 		remove_control_from_bottom_panel(file_system)
-		yield(get_tree(), "idle_frame")
 		add_control_to_dock(EditorPlugin.DOCK_SLOT_LEFT_BR, file_system)
-		
-		# Setup vertical container
-		box_container = VBoxContainer.new()
-		split_container = VSplitContainer.new()
 		file_system.rect_min_size = Vector2.ONE
-	
-	# Refrences
-	var headder: Control = file_system.get_child(0) # BoxContainer
-	var body: Control = file_system.get_child(3) # SplitContainer
-	
-	# Apply new container
-	headder.replace_by(box_container, true)
-	body.replace_by(split_container, true)
-	
-	# Child Node Refrences
-	var split_button: Button = box_container.get_child(0).get_child(4)
-	var file_tree: Tree = split_container.get_child(0)
-	var file_item: VBoxContainer = split_container.get_child(1)
-	var item_view: ToolButton = file_item.get_child(0).get_child(2)
+		
+		# Apply new container
+		file_system_box.replace_by(file_system_vbox, true)
+		file_system_split.replace_by(file_system_vsplit, true)
 	
 	# adjustment
-	split_button.visible = !docked
-	split_button.pressed = docked
-	item_view.pressed = !docked
+	file_system_box_split_view.pressed = docked
+	file_system_split_item_view.pressed = !docked
 	
-	file_tree.size_flags_stretch_ratio = TREE_STRETCH_RATIO
-	file_tree.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	file_item.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	file_system_box.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	file_system_split.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	file_system_split.size_flags_vertical = Control.SIZE_EXPAND_FILL
 	
-	box_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	split_container.size_flags_horizontal = Control.SIZE_EXPAND_FILL
-	split_container.size_flags_vertical = Control.SIZE_EXPAND_FILL
+	file_system_split_tree.size_flags_stretch_ratio = TREE_STRETCH_RATIO
+	file_system_split_tree.size_flags_horizontal = Control.SIZE_EXPAND_FILL
+	file_system_split_item.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	
-	for item in box_container.get_children():
+	for item in file_system_box.get_children():
 		item.size_flags_horizontal = Control.SIZE_EXPAND_FILL
 	
 	_processing = false
